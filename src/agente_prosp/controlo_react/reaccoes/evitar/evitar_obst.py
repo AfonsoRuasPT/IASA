5cd20dfc9cb8e0a84586d22ce362911c745f771b
AfonsoRuasPT <afonsotruas@gmail.com>
2026-05-07 20:01:16 +0100

    Parte2 - evitar

---
+++ src/agente_prosp/controlo_react/reaccoes/evitar/evitar_obst.py
@@ -0,0 +1,21 @@
+from ecr.reaccao import Reaccao
+from .resposta_evitar import RespostaEvitar
+from .estimulo_obs import EstminuloObs
+
+class EvitarObst(Reaccao):
+    """
+    Evitar obstaculo é issencioal para o bom funcionamento e para a continuidade do jogo por issp tera uma hirarquia media ficando
+    no meio de AproximarAlvo e Explorar
+    Este comportamento deve verificar a distancia que esta de cada obstaculo, se essa distancia for irgual a 1 o agente deve rodar 90
+    graus para a esquerda ou para a direita, com o conhecimento de que se ao rodar se depara com outro obstaculo deve rodar 
+    para o mesmo lado
+    """
+
+
+    """
+    Segundo a nossa arquitetura uma reaccao, é um modulo que associa um estimulo e uma resposta, 
+    como estamos na class EvitarObst, o estumulo é o EstimuloObs, que é a class que deteta a distancia dos obstaculos,
+    e a rtesposta é a RespostaEvitar, que é class que tem a accao de rodar o aguente para evitar o obstaculo.
+    """
+    def __init__(self):
+        super().__init__(EstminuloObs, RespostaEvitar(None))
